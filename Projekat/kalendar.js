4fb60b81688bef24d31f0e1e9f74b83b3661572d
amusic5 <amusic5@etf.unsa.ba>
2019-11-23 20:01:41 +0100

    Zadaci bez testova

---
+++ Projekat/kalendar.js
@@ -0,0 +1,187 @@
+let Kalendar = ( function() {
+	var stalni;
+	var privremeni;
+
+	function provjeriSemestar (semestar,mjesec) {
+		if(semestar != "ljetni" && semestar != "zimski" && (mjesec <= 0 || mjesec > 12)) return false;
+		if (semestar === "ljetni" && mjesec>=2 && mjesec <=6)
+			return true;
+		if (semestar === "zimski" && (mjesec>=10 || mjesec === 1) )
+			return true;
+		return false;
+	}
+
+	function provjeriMjesec (privMjesec,mjesec){
+		if(mjesec <=0 || mjesec >12) return false;
+		if (mjesec != 7 && mjesec != 8 && mjesec != 9 && privMjesec===mjesec) return true;
+		return false;
+	}
+
+	function obojiUZelene (kalendarRef) {
+		for(var i=1; i<kalendarRef.rows.length;i++){
+			for(var j=0;j<=6;j++){
+				kalendarRef.rows[i].cells[j].className="slobodna";
+			}
+		}
+	}
+	function provjeriVrijeme (pocetak, kraj) {
+
+		if(pocetak === kraj || pocetak.length != 5 || kraj.length != 5) return false;
+		var pocetakSati = parseInt(pocetak.substring(0,2),10);
+		var krajSati = parseInt(kraj.substring(0,2),10);
+		var pocetakMinute = parseInt(pocetak.substring(3,5),10);
+		var krajMinute = parseInt(kraj.substring(3,5),10);
+		var pocetakDvotacka = pocetak.substring(2,3);
+		var krajDvotacka = kraj.substring(2,3);
+
+		if(pocetakSati>krajSati) return false;
+
+		if(pocetakSati === krajSati){
+			if(pocetakMinute > krajMinute) return false;
+		}
+
+		if(pocetakDvotacka != ":" || krajDvotacka !=":" || pocetakSati < 0 || pocetakSati > 23 || 
+			krajSati < 0 || krajSati > 23 || pocetakMinute < 0 || pocetakMinute > 59 || krajMinute < 0 || 
+			krajMinute > 59) return false;
+
+			return true;
+	}
+
+	function obojiZauzecaImpl (kalendarRef, mjesec, sala, pocetak, kraj) {
+		obojiUZelene(kalendarRef);
+		for(var i = 0; i<stalni.length; i++){
+			if(provjeriVrijeme(pocetak,kraj) && sala === stalni[i].naziv && provjeriSemestar(stalni[i].semestar,mjesec+1) && 
+				pocetak === stalni[i].pocetak && kraj === stalni[i].kraj ) obojiStalne(kalendarRef,stalni[i]); 
+		}
+
+	for (var i = 0; i < privremeni.length; i++) {
+		var mjesecPrivremeni = parseInt(privremeni[i].datum.substring(3,5),10);
+		if(provjeriVrijeme(pocetak,kraj) && sala === privremeni[i].naziv && provjeriMjesec(mjesecPrivremeni,mjesec+1) &&
+			pocetak === privremeni[i].pocetak && kraj === privremeni[i].kraj ) obojiPrivremene(kalendarRef,privremeni[i]); 
+	}
+}
+function obojiStalne(kalendarRef,termin){
+	for (var i=1; i < kalendarRef.rows.length; i++) {
+		kalendarRef.rows[i].cells[termin.dan].className="zauzeta";
+	}
+}
+
+function provjeriInnerHtml(a,b){
+	var a = parseInt(a,10);
+	if(a===b) return true;
+	return false;
+}
+
+function obojiPrivremene(kalendarRef,termin){
+	var dan = parseInt(termin.datum.substring(0,2),10);
+	for (var i=1; i < kalendarRef.rows.length; i++) {
+		for (var j=0; j<= 6; j++) {
+			var innerHTML = kalendarRef.rows[i].cells[j].innerHTML;
+			if(provjeriInnerHtml(innerHTML,dan)) kalendarRef.rows[i].cells[j].className="zauzeta";
+		}
+	}
+}
+
+function ucitajPodatkeImpl (periodicna, redovna) {
+	stalni = periodicna;
+	privremeni = redovna;
+}
+
+function iscrtajKalendarImpl (kalendarRef, mjesec){
+	brisi(kalendarRef);
+	var imeMjeseci = ['Januar','Februar','Mart','April','Maj','Juni','Juli','August','Septembar','Oktobar','Novembar','Decembar'];
+	var datum = new Date();
+	var godina = datum.getFullYear(); 
+	var brojDana = new Date(godina, mjesec+1, 0).getDate();
+	var temp = new Date(godina, mjesec).toDateString();
+	var prviDan = temp.substring(0, 3);    
+	var imeDana = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
+	var danUSedmici = imeDana.indexOf(prviDan);   
+	kalendarRef.caption.innerHTML=imeMjeseci[mjesec];
+	var ispisao = 1;
+	var red = 1;
+	for (var j = 1; j <= 7; j++) {
+		var row = kalendarRef.insertRow(j);
+		for (var i=0; i < 7; i++) {
+			var cell = row.insertCell(i);
+			if ( (i < danUSedmici && j===1 ) || ispisao > brojDana){
+				cell.className="slobodna";
+				cell.innerHTML="";
+			}
+			else {
+				cell.className = "slobodna";
+				cell.innerHTML = ispisao;
+				ispisao++;
+			}
+		} 
+		if (ispisao > brojDana) break;
+	}
+}return {
+	obojiZauzeca: obojiZauzecaImpl,
+	ucitajPodatke: ucitajPodatkeImpl,
+	iscrtajKalendar: iscrtajKalendarImpl
+}
+}
+())
+;
+
+var kalendar = document.getElementById('tabelaKalendar');
+var datum = new Date();
+var mjesec = datum.getMonth();   
+
+window.onload = function(){
+	Kalendar.iscrtajKalendar(kalendar, mjesec);
+
+	var privremeni = [{datum: "04.04.2019", pocetak: "09:00", kraj: "12:00", naziv: "MA", predavac: "Tom"},
+	{datum: "21.11.2019", pocetak: "09:00", kraj: "10:00", naziv: "MA", predavac: "Pepeljuga"},
+	{datum: "22.11.2019", pocetak: "15:00", kraj: "18:00", naziv: "MA", predavac: "Merlin"},
+	{datum: "15.11.2019", pocetak: "10:00", kraj: "11:00", naziv: "EE1", predavac: "Mumi"},
+	{datum: "15.11.2019", pocetak: "10:00", kraj: "11:00", naziv: "EE2 ", predavac: "Mumi"},
+	{datum: "20.10.2019", pocetak: "09:00", kraj: "12:00", naziv: "VA", predavac: "Smrdo"},
+	{datum: "01.01.2019", pocetak: "14:00", kraj: "16:00", naziv: "MA", predavac: "Gargamel"},
+	{datum: "15.11.2019", pocetak: "15:00", kraj: "18:00", naziv: "MA", predavac: "Winx"},
+	{datum: "20.10.2019", pocetak: "09:00", kraj: "12:00", naziv: "VA", predavac: "Mumi"},
+	{datum: "01.05.2019", pocetak: "11:00", kraj: "12:00", naziv: "VA", predavac: "Smrdo"},
+	{datum: "01.01.2019", pocetak: "14:00", kraj: "16:00", naziv: "MA", predavac: "Gargamel"},
+	{datum: "14.12.2019", pocetak: "15:00", kraj: "18:00", naziv: "EE2", predavac: "Winx"}];
+
+	var stalni  = [{dan: 4, semestar: "ljetni", pocetak: "09:00", kraj:"12:00", naziv: "MA", predavac: "MikiMaus"},
+	{dan: 4, semestar: "ljetni", pocetak: "12:00", kraj: "15:00", naziv: "VA", predavac: "Dzery"},
+	{dan: 6, semestar: "zimski", pocetak: "15:00", kraj: "18:00", naziv: "0-02", predavac: "Crvenkapica"},
+	{dan: 3, semestar: "zimski", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Njuso"},
+	{dan: 1, semestar: "ljetni", pocetak: "14:00", kraj: "16:30", naziv: "EE1", predavac: "MalaMu"},
+	{dan: 5, semestar: "zimski", pocetak: "10:00", kraj: "11:00", naziv: "VA", predavac: "Pikacu"},
+	{dan: 2, semestar: "ljetni", pocetak: "12:00", kraj: "14:00", naziv: "0-01", predavac: "PajaPatak"}];
+
+	Kalendar.ucitajPodatke(stalni,privremeni);
+	osvjeziSale();
+}
+
+function Sljedeci() {
+	if (mjesec < 11) {
+		mjesec++;
+		Kalendar.iscrtajKalendar(kalendar, mjesec);
+		osvjeziSale();
+	}
+}
+
+function brisi(kalendar) {
+	var brojRedova = kalendar.rows.length;
+	for (var i = 0; i < brojRedova - 1; i++)
+		kalendar.deleteRow(1);
+}
+
+function Prethodni() {
+	if (mjesec > 0) {
+		mjesec--;
+		Kalendar.iscrtajKalendar(kalendar, mjesec);
+		osvjeziSale();
+	}
+}
+
+function osvjeziSale() {
+	var a = document.getElementById("listaSala").value;
+	var b = document.getElementById("pocetak").value;
+	var c = document.getElementById("kraj").value;
+	Kalendar.obojiZauzeca(kalendar,mjesec,a,b,c);
+}
